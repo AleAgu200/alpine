e86cfe7f80e2fb4b01ab6e2a05b1f257d61c33a8
AleAgu200 <39972765+AleAgu200@users.noreply.github.com>
2024-01-27 16:10:51 -0600

    added new file

--- app.config.ts
+++ app.config.ts
@@ -1,6 +1,6 @@
 export default defineAppConfig({
   alpine: {
-    title: 'Alpine',
+    title: 'Hello',
     description: 'The minimalist blog theme',
     image: {
       src: '/social-card-preview.png',
